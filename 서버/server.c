6f799a24fc8bc0acf7881633c54f52fc377473b3
hyunjun <nhj2927@naver.com>
2021-06-04 18:27:03 +0900

    update code

--- 서버/server.c
+++ 서버/server.c
@@ -1,4 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#pragma comment(lib,"ws2_32.lib")
 #include<stdio.h>
 #include<stdlib.h>
 #include<process.h>
@@ -19,7 +20,7 @@
 typedef struct {
 	SOCKET hClntSock;
 	SOCKADDR_IN clntAdr;
-} PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
+} PER_HANDLE_DATA, * LPPER_HANDLE_DATA;
 
 //buffer info
 typedef struct {
@@ -27,7 +28,7 @@ typedef struct {
 	WSABUF wsaBuf;
 	char buffer[BUF_SIZE];
 	int rwMode;
-} PER_IO_DATA, *LPPER_IO_DATA;
+} PER_IO_DATA, * LPPER_IO_DATA;
 
 DWORD WINAPI threadMain(LPVOID completionPortIO);
 void sendMsgToAll(char* message, DWORD len);
@@ -37,30 +38,41 @@ int mafia1, mafia2;
 int clntCnt = 0;
 int mafiaCnt = 2;
 int citizenCnt = 4;
+int vote[MAX_CLNT] = { 0 };
+int voteCount;
 SOCKET clntSocks[MAX_CLNT];
+CRITICAL_SECTION cs1, cs2;
 
 void setConnectedMsg(char* msg) {
 	msg[0] = 'H';
 	msg[1] = 1;
-	msg[2] = clntCnt - 1;
+	msg[2] = clntCnt;
 	msg[3] = 0;
 }
 
-void setChatMsg(char* msg, char clntNum, char* chatMsg) {
+void setDisconnectedMsg(char* msg) {
+	msg[0] = 'I';
+	msg[1] = 1;
+	msg[2] = clntCnt;
+	msg[3] = 0;
+}
+
+void setChatMsg(char* msg, int clntNum, char* chatMsg) {
 	msg[0] = 'B';
 	msg[1] = strlen(chatMsg) + 2;
 	msg[2] = clntNum;
 	strcpy(msg + 3, chatMsg);
 }
 
-void setGameStartMsg(char* msg, char role) {
+void setGameStartMsg(char* msg, char role, char clntNum) {
 	msg[0] = 'A';
-	msg[1] = 1;
-	msg[2] = role;
-	msg[3] = 0;
+	msg[1] = 2;
+	msg[2] = clntNum;
+	msg[3] = role;
+	msg[4] = 0;
 }
 
-void setDayTrigger(char *msg, char result) {
+void setDayTrigger(char* msg, char result) {
 	msg[0] = 'C';
 	msg[1] = 5;
 	msg[2] = result;
@@ -89,9 +101,7 @@ int isGameEnd() {
 	if (mafiaCnt == 0 || mafiaCnt >= citizenCnt) {
 		return TRUE;
 	}
-	else {
-		return FALSE;
-	}
+	else return FALSE;
 }
 
 int main() {
@@ -108,9 +118,11 @@ int main() {
 	flags = 0;
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		errorHandling("WSAStartup() erroor!");
+		errorHandling("WSAStartup() error!");
 	}
 
+	InitializeCriticalSection(&cs1);
+	InitializeCriticalSection(&cs2);
 	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 	GetSystemInfo(&sysInfo);
 	for (i = 0; i < sysInfo.dwNumberOfProcessors; i++) {
@@ -126,14 +138,16 @@ int main() {
 	bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr));
 	listen(hServSock, 5);
 
-	for (i = 0; i < 6; i++) {
+	while (clntCnt < 6) {
 		SOCKET hClntSock;
 		SOCKADDR_IN clntAdr;
 		char message[BUF_SIZE];
 		int addrLen = sizeof(clntAdr);
 
 		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+		EnterCriticalSection(&cs1);
 		clntSocks[clntCnt++] = hClntSock;
+		LeaveCriticalSection(&cs1);
 
 		handleInfo = (LPPER_HANDLE_DATA)malloc(sizeof(PER_HANDLE_DATA));
 		handleInfo->hClntSock = hClntSock;
@@ -152,8 +166,10 @@ int main() {
 		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1, &recvBytes, &flags, &(ioInfo->overlapped), NULL);
 	}
 
-	mafia1 = rand() % clntCnt;
-	mafia2 = rand() % clntCnt;
+	Sleep(3000);
+	printf("게임 시작!");
+	mafia1 = (rand() % clntCnt);
+	mafia2 = (rand() % clntCnt);
 
 	while (mafia1 == mafia2) {
 		mafia2 = rand() % clntCnt;
@@ -165,26 +181,18 @@ int main() {
 		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 
 		if (i == mafia1) {
-			setGameStartMsg(message, MAFIA1);
-			ioInfo->wsaBuf.len = strlen(message);
-			ioInfo->wsaBuf.buf = message;
-			ioInfo->rwMode = WRITE;;
-			WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+			setGameStartMsg(message, MAFIA1, i + 1);
+			sendMsgToAll(message, strlen(message));
 		}
 		else if (i == mafia2) {
-			setGameStartMsg(message, MAFIA2);
-			ioInfo->wsaBuf.len = strlen(message);
-			ioInfo->wsaBuf.buf = message;
-			ioInfo->rwMode = WRITE;;
-			WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+			setGameStartMsg(message, MAFIA2, i + 1);
+			sendMsgToAll(message, strlen(message));
 		}
 		else {
-			setGameStartMsg(message, CITIZEN);
-			ioInfo->wsaBuf.len = strlen(message);
-			ioInfo->wsaBuf.buf = message;
-			ioInfo->rwMode = WRITE;;
-			WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+			setGameStartMsg(message, CITIZEN, i + 1);
+			sendMsgToAll(message, strlen(message));
 		}
+
 	}
 
 	while (clntCnt > 0);
@@ -199,10 +207,8 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 	LPPER_HANDLE_DATA handleInfo;
 	LPPER_IO_DATA ioInfo;
 	DWORD flags;
-	char temp[BUFSIZ];
-	int i, state, index, clntNum, currentClntNum = clntCnt, count = 0, deathPerson = -1, isEqual = 0;
-	int countVote[MAX_CLNT] = { 0, };
-
+	char message[BUFSIZ];
+	int i, state, index, clntNum, currentClntNum, deathPerson = -1, isEqual = 0;
 
 	flags = 0;
 
@@ -216,8 +222,12 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 				{
 					if (sock == clntSocks[i])
 					{
+						setDisconnectedMsg(message);
+						sendMsgToAll(message, strlen(message));
+						EnterCriticalSection(&cs1);
 						while (i++ < clntCnt - 1)
 							clntSocks[i] = clntSocks[i + 1];
+						LeaveCriticalSection(&cs1);
 						break;
 					}
 				}
@@ -235,26 +245,29 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 			case 'B':
 				clntNum = ioInfo->buffer[2];
 				ioInfo->buffer[ioInfo->buffer[1] + 2] = 0;
-				strcpy(temp, ioInfo->buffer + 3);
-				setChatMsg(ioInfo->wsaBuf.buf, clntNum, temp);
+				strcpy(message, ioInfo->buffer + 3);
+				setChatMsg(ioInfo->wsaBuf.buf, clntNum, message);
 				sendMsgToAll(ioInfo->wsaBuf.buf, strlen(ioInfo->wsaBuf.buf));
 				break;
 			case 'E':
-				index = atoi(ioInfo->buffer[2]);
-				countVote[index]++;
-				count++;
-				if (count == currentClntNum) {
+				currentClntNum = mafiaCnt + citizenCnt;
+				index = ioInfo->buffer[2];
+				EnterCriticalSection(&cs2);
+				vote[index]++;
+				voteCount++;
+				LeaveCriticalSection(&cs2);
+				if (voteCount == currentClntNum) {
 					int max = 0;
 					for (i = 0; i < clntCnt; i++) {
-						if (countVote[i] > max) {
-							max = countVote[i];
+						if (vote[i] > max) {
+							max = vote[i];
 							deathPerson = i;
 							isEqual = 0;
 						}
-						else if (countVote[i] == max) {
+						else if (vote[i] == max) {
 							isEqual = 1;
 						}
-						countVote[i] = 0;
+						vote[i] = 0;
 					}
 
 					if (isEqual) {
@@ -262,11 +275,9 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 					}
 
 					if (deathPerson == mafia1 || deathPerson == mafia2) {
-						currentClntNum--;
 						mafiaCnt--;
 					}
 					else if (deathPerson != -1) {
-						currentClntNum--;
 						citizenCnt--;
 					}
 
@@ -279,10 +290,12 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 					}
 					sendMsgToAll(ioInfo->wsaBuf.buf, strlen(ioInfo->wsaBuf.buf));
 
-					count = 0;
+					memset(vote, 0, BUF_SIZE);
+					voteCount = 0;
 				}
 				break;
 			case 'F':
+				printf("vote\n");
 				citizenCnt--;
 				if (isGameEnd()) {
 					setGameEndTrigger(ioInfo->wsaBuf.buf);
@@ -294,7 +307,7 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 				sendMsgToAll(ioInfo->wsaBuf.buf, strlen(ioInfo->wsaBuf.buf));
 				break;
 			}
-			
+
 			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 			ioInfo->wsaBuf.len = BUF_SIZE;
@@ -303,12 +316,6 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
 		}
 		else {
-			if (ioInfo->wsaBuf.buf[0] == 'A') {
-				setDayTrigger(ioInfo->wsaBuf.buf, -2);
-				ioInfo->wsaBuf.len = strlen(ioInfo->wsaBuf.buf);
-				ioInfo->rwMode = WRITE;;
-				WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-			}
 			free(ioInfo);
 		}
 	}
@@ -320,7 +327,7 @@ void sendMsgToAll(char* message, DWORD len) {
 	WSABUF wsaBuf;
 	LPPER_IO_DATA ioInfo;
 	int i, recvBytes, a;
-	
+
 	for (i = 0; i < clntCnt; i++) {
 		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
