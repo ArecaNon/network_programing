4606ee78855f71fdf37483b29725d8f6add80e3d
NaHyunJun <nhj2927@naver.com>
2021-06-09 16:24:37 +0900

    update code

--- 서버/server.c
+++ 서버/server.c
@@ -1,121 +1,35 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<process.h>
-#include<winsock2.h>
-#include<windows.h>
-
-#define BUF_SIZE 104
-#define READ 3
-#define WRITE 5
-#define MAX_CLNT 256
-#define MAFIA1 1
-#define MAFIA2 2
-#define CITIZEN 3
-#define TRUE 1
-#define FALSE 0
-
-//socket info
-typedef struct {
-	SOCKET hClntSock;
-	SOCKADDR_IN clntAdr;
-} PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
-
-//buffer info
-typedef struct {
-	OVERLAPPED overlapped;
-	WSABUF wsaBuf;
-	char buffer[BUF_SIZE];
-	int rwMode;
-} PER_IO_DATA, *LPPER_IO_DATA;
-
-DWORD WINAPI threadMain(LPVOID completionPortIO);
-void sendMsgToAll(char* message, DWORD len);
-void errorHandling(char* message);
-
-int mafia1, mafia2;
-int clntCnt = 0;
-int mafiaCnt = 2;
-int citizenCnt = 4;
-SOCKET clntSocks[MAX_CLNT];
-
-void setConnectedMsg(char* msg) {
-	msg[0] = 'H';
-	msg[1] = 1;
-	msg[2] = clntCnt - 1;
-	msg[3] = 0;
-}
-
-void setChatMsg(char* msg, char clntNum, char* chatMsg) {
-	msg[0] = 'B';
-	msg[1] = strlen(chatMsg) + 2;
-	msg[2] = clntNum;
-	strcpy(msg + 3, chatMsg);
-}
-
-void setGameStartMsg(char* msg, char role) {
-	msg[0] = 'A';
-	msg[1] = 1;
-	msg[2] = role;
-	msg[3] = 0;
-}
-
-void setDayTrigger(char *msg, char result) {
-	msg[0] = 'C';
-	msg[1] = 5;
-	msg[2] = result;
-	msg[3] = 30;
-	msg[4] = 10;
-	msg[5] = 0;
-}
-
-void setNightTrigger(char* msg, char result) {
-	msg[0] = 'D';
-	msg[1] = 5;
-	msg[2] = result;
-	msg[3] = 20;
-	msg[4] = 5;
-	msg[5] = 0;
-}
-
-void setGameEndTrigger(char* msg) {
-	msg[0] = 'G';
-	msg[1] = 3;
-	msg[2] = 1;
-	msg[3] = 0;
-}
-
-int isGameEnd() {
-	if (mafiaCnt == 0 || mafiaCnt >= citizenCnt) {
-		return TRUE;
-	}
-	else {
-		return FALSE;
-	}
-}
+#include "server.h"
 
 int main() {
 	WSADATA wsaData;
 	HANDLE hComPort;
 	SYSTEM_INFO sysInfo;
-	LPPER_IO_DATA ioInfo;
-	LPPER_HANDLE_DATA handleInfo;
+	LPPER_IO_DATA ioInfo; //buffer info
+	LPPER_HANDLE_DATA handleInfo; //socket info
 
 	SOCKET hServSock;
 	SOCKADDR_IN servAdr;
-	int recvBytes, i, flags;
-
-	flags = 0;
+	char input;
+	int recvBytes, i, flags = 0;
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		errorHandling("WSAStartup() erroor!");
+		errorHandling("WSAStartup() error!");
 	}
+
+	//cs 초기화
+	InitializeCriticalSection(&cs[0]);
+	InitializeCriticalSection(&cs[1]);
+	InitializeCriticalSection(&cs[2]);
+
+	srand(time(NULL)); //seed값 설정
 
 	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 	GetSystemInfo(&sysInfo);
+	//thread 실행
 	for (i = 0; i < sysInfo.dwNumberOfProcessors; i++) {
-		_beginthreadex(NULL, 0, threadMain, (LPVOID)hComPort, 0, NULL);
+		_beginthreadex(NULL, 0, mainThread, (LPVOID)hComPort, 0, NULL);
 	}
+	_beginthreadex(NULL, 0, gameManageThread, NULL, 0, NULL);
 
 	hServSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	memset(&servAdr, 0, sizeof(servAdr));
@@ -126,23 +40,31 @@ int main() {
 	bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr));
 	listen(hServSock, 5);
 
-	for (i = 0; i < 6; i++) {
+	while (1) {
 		SOCKET hClntSock;
 		SOCKADDR_IN clntAdr;
 		char message[BUF_SIZE];
 		int addrLen = sizeof(clntAdr);
 
 		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+		EnterCriticalSection(&cs[0]);
 		clntSocks[clntCnt++] = hClntSock;
+		LeaveCriticalSection(&cs[0]);
 
 		handleInfo = (LPPER_HANDLE_DATA)malloc(sizeof(PER_HANDLE_DATA));
 		handleInfo->hClntSock = hClntSock;
 		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
 
 		CreateIoCompletionPort((HANDLE)hClntSock, hComPort, (DWORD)handleInfo, 0);
-
-		setConnectedMsg(message);
-		sendMsgToAll(message, strlen(message));
+		
+		//방 현재인원 정보 클라이언트로 전송
+		setRoomInfoMsg(message);
+		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+		ioInfo->wsaBuf.len = 4;
+		ioInfo->wsaBuf.buf = message;
+		ioInfo->rwMode = WRITE;
+		WSASend(hClntSock, &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
 
 		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
@@ -152,72 +74,164 @@ int main() {
 		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1, &recvBytes, &flags, &(ioInfo->overlapped), NULL);
 	}
 
-	mafia1 = rand() % clntCnt;
-	mafia2 = rand() % clntCnt;
-
-	while (mafia1 == mafia2) {
-		mafia2 = rand() % clntCnt;
-	}
-
-	for (i = 0; i < clntCnt; i++) {
-		char message[BUF_SIZE];
-
-		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
-
-		if (i == mafia1) {
-			setGameStartMsg(message, MAFIA1);
-			ioInfo->wsaBuf.len = strlen(message);
-			ioInfo->wsaBuf.buf = message;
-			ioInfo->rwMode = WRITE;;
-			WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-		}
-		else if (i == mafia2) {
-			setGameStartMsg(message, MAFIA2);
-			ioInfo->wsaBuf.len = strlen(message);
-			ioInfo->wsaBuf.buf = message;
-			ioInfo->rwMode = WRITE;;
-			WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-		}
-		else {
-			setGameStartMsg(message, CITIZEN);
-			ioInfo->wsaBuf.len = strlen(message);
-			ioInfo->wsaBuf.buf = message;
-			ioInfo->rwMode = WRITE;;
-			WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-		}
-	}
-
-	while (clntCnt > 0);
-
 	return 0;
 }
 
-DWORD WINAPI threadMain(LPVOID pComPort) {
+// 6명 채워지면 역할 부여 후 게임 시작
+DWORD WINAPI gameManageThread() {
+	int i, mafia1, mafia2;
+	LPPER_IO_DATA ioInfo;
+
+	while (1) {
+		if (!gameStart[0] && clntCntOfRoom[0] == MAX_PLAYER) { //1번방 게임시작전이고 인원수가 채워졌을 때
+
+			//게임시작전 초기화
+			gameStart[0] = TRUE; 
+			mafiaCnt[0] = MAX_MAFIA;
+			citizenCnt[0] = MAX_CITIZEN;
+			voteCnt[0] = 0;
+			memset(vote[0], 0, sizeof(int) * MAX_PLAYER);
+			
+			//마피아 설정
+			mafia1 = rand() % MAX_PLAYER;
+			mafia2 = rand() % MAX_PLAYER;
+
+			//마피아 index가 같을때
+			while (mafia1 == mafia2) {
+				mafia2 = rand() % MAX_PLAYER;
+			}
+			mafiaIndex[0][0] = mafia1;
+			mafiaIndex[0][1] = mafia2;
+			Sleep(1000);
+
+			//1번방 유저들에게 게임시작 메세지 전송
+			for (i = 0; i < MAX_PLAYER; i++){
+				char message[BUF_SIZE];
+
+				ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+				memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+				Sleep(100);
+				
+				if (i == mafia1) {
+					setGameStartMsg(message, MAFIA1, i + 1);
+					ioInfo->wsaBuf.len = strlen(message);
+					ioInfo->wsaBuf.buf = message;
+					ioInfo->rwMode = WRITE;
+					WSASend(clntSocksOfRoom[0][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+				}
+				else if (i == mafia2) {
+					setGameStartMsg(message, MAFIA2, i + 1);
+					ioInfo->wsaBuf.len = strlen(message);
+					ioInfo->wsaBuf.buf = message;
+					ioInfo->rwMode = WRITE;
+					WSASend(clntSocksOfRoom[0][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+				}
+				else {
+					setGameStartMsg(message, CITIZEN, i + 1);
+					ioInfo->wsaBuf.len = strlen(message);
+					ioInfo->wsaBuf.buf = message;
+					ioInfo->rwMode = WRITE;
+					WSASend(clntSocksOfRoom[0][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+				}
+			}
+		}
+		else if (!gameStart[1] && clntCntOfRoom[1] == MAX_PLAYER) { //2번방 게임시작전이고 인원수가 채워졌을 때
+			gameStart[1] = TRUE;
+			mafiaCnt[1] = MAX_MAFIA;
+			citizenCnt[1] = MAX_CITIZEN;
+			voteCnt[1] = 0;
+			memset(vote[1], 0, sizeof(int) * MAX_PLAYER);
+
+			mafia1 = rand() % MAX_PLAYER;
+			mafia2 = rand() % MAX_PLAYER;
+
+			while (mafia1 == mafia2) {
+				mafia2 = rand() % MAX_PLAYER;
+			}
+			mafiaIndex[1][0] = mafia1;
+			mafiaIndex[1][1] = mafia2;
+			Sleep(1000);
+
+			//2번방 유저들에게 게임시작 메세지 전송
+			for (i = 0; i < MAX_PLAYER; i++) {
+				char message[BUF_SIZE];
+
+				ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+				memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+				Sleep(100);
+
+				if (i == mafia1) {
+					setGameStartMsg(message, MAFIA1, i + 1);
+					ioInfo->wsaBuf.len = strlen(message);
+					ioInfo->wsaBuf.buf = message;
+					ioInfo->rwMode = WRITE;
+					WSASend(clntSocksOfRoom[1][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+				}
+				else if (i == mafia2) {
+					setGameStartMsg(message, MAFIA2, i + 1);
+					ioInfo->wsaBuf.len = strlen(message);
+					ioInfo->wsaBuf.buf = message;
+					ioInfo->rwMode = WRITE;
+					WSASend(clntSocksOfRoom[1][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+				}
+				else {
+					setGameStartMsg(message, CITIZEN, i + 1);
+					ioInfo->wsaBuf.len = strlen(message);
+					ioInfo->wsaBuf.buf = message;
+					ioInfo->rwMode = WRITE;
+					WSASend(clntSocksOfRoom[1][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+DWORD WINAPI mainThread(LPVOID pComPort) {
 	HANDLE hComPort = (HANDLE)pComPort;
 	SOCKET sock;
 	DWORD bytesTrans;
 	LPPER_HANDLE_DATA handleInfo;
 	LPPER_IO_DATA ioInfo;
 	DWORD flags;
-	char temp[BUFSIZ];
-	int i, state, index, clntNum, currentClntNum = clntCnt, count = 0, deathPerson = -1, isEqual = 0;
-	int countVote[MAX_CLNT] = { 0, };
-
+	char message[BUF_SIZE];
+	int i, j, state, index, clntNum, currentClntNum, deathPerson, isEqual, max;
+	int roomNum, result;
 
 	flags = 0;
 
 	while (1) {
 		GetQueuedCompletionStatus(hComPort, &bytesTrans, (LPWORD)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
 		sock = handleInfo->hClntSock;
+		roomNum = checkRoomNum(sock); //몇번방 클라이언트인지 확인
 
 		if (ioInfo->rwMode == READ) {
 			if (bytesTrans == 0) { //EOF
-				for (i = 0; i < clntCnt; i++)   //remove disconnected client
+				for (i = 0; i < clntCnt; i++)   //연결해제된 클라이언트 삭제
 				{
 					if (sock == clntSocks[i])
 					{
-						while (i++ < clntCnt - 1)
+						EnterCriticalSection(&cs[0]);
+						while (i++ < clntCnt - 1) {
 							clntSocks[i] = clntSocks[i + 1];
+						}
+						//클라이언트가 있던방에서 클라이언트 삭제
+						for (j = 0; j < MAX_PLAYER; j++) {
+							if (sock == clntSocksOfRoom[roomNum][j]) {
+								clntSocksOfRoom[roomNum][j] = 0;
+								setDisconnectedMsg(message, j + 1);
+								if (clntCntOfRoom[roomNum] > 0) {
+									clntCntOfRoom[roomNum]--;
+								}
+							}
+						}
+						//게임이 시작되고 클라이언트 수가 0이되면 게임종료로 설정
+						if (gameStart[roomNum] && !clntCntOfRoom[roomNum]) {
+							gameStart[roomNum] = FALSE;
+						}
+						LeaveCriticalSection(&cs[0]);
+						//연결해제 메세지 전송
+						sendMsgToAll(message, roomNum, strlen(message));
 						break;
 					}
 				}
@@ -232,69 +246,106 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 			state = ioInfo->buffer[0];
 
 			switch (state) {
-			case 'B':
+			case 'B': //채팅 메세지 전송
 				clntNum = ioInfo->buffer[2];
 				ioInfo->buffer[ioInfo->buffer[1] + 2] = 0;
-				strcpy(temp, ioInfo->buffer + 3);
-				setChatMsg(ioInfo->wsaBuf.buf, clntNum, temp);
-				sendMsgToAll(ioInfo->wsaBuf.buf, strlen(ioInfo->wsaBuf.buf));
+				strcpy(message, ioInfo->buffer + 3);
+				setChatMsg(ioInfo->wsaBuf.buf, clntNum, message);
+				sendMsgToAll(ioInfo->wsaBuf.buf, roomNum, strlen(ioInfo->wsaBuf.buf));
 				break;
-			case 'E':
-				index = atoi(ioInfo->buffer[2]);
-				countVote[index]++;
-				count++;
-				if (count == currentClntNum) {
-					int max = 0;
-					for (i = 0; i < clntCnt; i++) {
-						if (countVote[i] > max) {
-							max = countVote[i];
-							deathPerson = i;
-							isEqual = 0;
+			case 'E': //투표 정보 받아와서 결과 전송
+				currentClntNum = mafiaCnt[roomNum] + citizenCnt[roomNum]; //현재 살아있는 유저 수
+				index = ioInfo->buffer[2] - 1; //투표 정보
+				EnterCriticalSection(&cs[1]);
+				vote[roomNum][index]++;
+				voteCnt[roomNum]++;
+				LeaveCriticalSection(&cs[1]);
+				if (voteCnt[roomNum] == currentClntNum) { //투표가 완료되면
+					max = 0;
+					deathPerson = -1;
+					isEqual = FALSE;
+					//투표로 죽은사람 확인
+					for (i = 0; i < MAX_PLAYER; i++) {
+						if (vote[roomNum][i] > max) {
+							max = vote[roomNum][i];
+							deathPerson = i + 1;
+							isEqual = FALSE;
 						}
-						else if (countVote[i] == max) {
-							isEqual = 1;
+						else if (vote[roomNum][i] == max) {
+							isEqual = TRUE;
 						}
-						countVote[i] = 0;
+						vote[roomNum][i] = 0;
 					}
-
+					//투표가 동률인 경우 무효처리
 					if (isEqual) {
 						deathPerson = -1;
 					}
-
-					if (deathPerson == mafia1 || deathPerson == mafia2) {
-						currentClntNum--;
-						mafiaCnt--;
+					//결과가 마피아인 경우
+					if (deathPerson - 1 == mafiaIndex[roomNum][0] || deathPerson - 1 == mafiaIndex[roomNum][1]) {
+						mafiaCnt[roomNum]--;
 					}
+					//결과가 시민인 경우
 					else if (deathPerson != -1) {
-						currentClntNum--;
-						citizenCnt--;
+						citizenCnt[roomNum]--;
 					}
 
-					if (isGameEnd()) {
-						setGameEndTrigger(ioInfo->wsaBuf.buf);
+					//게임종료시 결과 전송
+					if (isGameEnd(roomNum)) {
+						if (mafiaCnt[roomNum] == 0) {
+							result = CITIZEN_WIN;
+						}
+						else {
+							result = MAFIA_WIN;
+						}
+						setGameEndTrigger(ioInfo->wsaBuf.buf, result);
 					}
-
+					//밤 트리거 메세지 전송
 					else {
-						setNightTrigger(ioInfo->wsaBuf.buf, (char)deathPerson);
+						setNightTrigger(ioInfo->wsaBuf.buf, deathPerson);
 					}
-					sendMsgToAll(ioInfo->wsaBuf.buf, strlen(ioInfo->wsaBuf.buf));
+					sendMsgToAll(ioInfo->wsaBuf.buf, roomNum, strlen(ioInfo->wsaBuf.buf));
 
-					count = 0;
+					memset(vote[roomNum], 0, sizeof(int) * MAX_PLAYER);
+					voteCnt[roomNum] = 0;
 				}
 				break;
-			case 'F':
-				citizenCnt--;
-				if (isGameEnd()) {
-					setGameEndTrigger(ioInfo->wsaBuf.buf);
+			case 'F': //마피아가 죽인대상 확인후 결과 전송
+				citizenCnt[roomNum]--;
+				//게임 종료시 결과 전송
+				if (isGameEnd(roomNum)) {
+					if (mafiaCnt[roomNum] == 0) {
+						result = CITIZEN_WIN;
+					}
+					else {
+						result = MAFIA_WIN;
+					}
+					setGameEndTrigger(ioInfo->wsaBuf.buf, result);
 				}
+				//낮 트리거 메세지 전송
 				else {
-					deathPerson = atoi(&(ioInfo->buffer[2]));
-					setDayTrigger(ioInfo->wsaBuf.buf, (char)deathPerson);
+					deathPerson = ioInfo->buffer[2];
+					setDayTrigger(ioInfo->wsaBuf.buf, deathPerson);
 				}
-				sendMsgToAll(ioInfo->wsaBuf.buf, strlen(ioInfo->wsaBuf.buf));
+				sendMsgToAll(ioInfo->wsaBuf.buf, roomNum, strlen(ioInfo->wsaBuf.buf));
+				break;
+			case 'K': //몇번방 입장할건지 확인후 입장
+				roomNum = ioInfo->buffer[2] - 1; //방 번호
+				EnterCriticalSection(&cs[2]);
+				//해당하는 방에 입장
+				for (i = 0; i < MAX_PLAYER; i++) {
+					if (!clntSocksOfRoom[roomNum][i]) {
+						setConnectedMsg(message, i + 1);
+						break;
+					}
+				}
+				clntSocksOfRoom[roomNum][i] = sock;
+				clntCntOfRoom[roomNum]++;
+				LeaveCriticalSection(&cs[2]);
+				//입장 메세지 전송
+				sendMsgToAll(message, roomNum, strlen(message));
 				break;
 			}
-			
+
 			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 			ioInfo->wsaBuf.len = BUF_SIZE;
@@ -303,12 +354,6 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
 		}
 		else {
-			if (ioInfo->wsaBuf.buf[0] == 'A') {
-				setDayTrigger(ioInfo->wsaBuf.buf, -2);
-				ioInfo->wsaBuf.len = strlen(ioInfo->wsaBuf.buf);
-				ioInfo->rwMode = WRITE;;
-				WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-			}
 			free(ioInfo);
 		}
 	}
@@ -316,21 +361,114 @@ DWORD WINAPI threadMain(LPVOID pComPort) {
 	return 0;
 }
 
-void sendMsgToAll(char* message, DWORD len) {
+//게임시작 메세지 설정
+void setGameStartMsg(char* msg, int role, int clntNum) {
+	msg[0] = 'A';
+	msg[1] = 4;
+	msg[2] = clntNum;
+	msg[3] = role;
+	msg[4] = DAY_CONFERENCE_TIME;
+	msg[5] = DAY_VOTE_TIME;
+	msg[6] = 0;
+}
+
+//채팅 메세지 설정
+void setChatMsg(char* msg, int clntNum, char* chatMsg) {
+	msg[0] = 'B';
+	msg[1] = strlen(chatMsg) + 1;
+	msg[2] = clntNum;
+	strcpy(msg + 3, chatMsg);
+}
+
+//낮 트리거 메세지 설정
+void setDayTrigger(char* msg, int result) {
+	msg[0] = 'C';
+	msg[1] = 1;
+	msg[2] = result;
+	msg[3] = 0;
+}
+
+//밤 트리거 메세지 설정
+void setNightTrigger(char* msg, int result) {
+	msg[0] = 'D';
+	msg[1] = 1;
+	msg[2] = result;
+	msg[3] = 0;
+}
+
+//게임 종료 메세지 설정
+void setGameEndTrigger(char* msg, int result) {
+	msg[0] = 'G';
+	msg[1] = 1;
+	msg[2] = result;
+	msg[3] = 0;
+}
+
+//연결 메세지 설정
+void setConnectedMsg(char* msg, int clntNum) {
+	msg[0] = 'H';
+	msg[1] = 1;
+	msg[2] = clntNum;
+	msg[3] = 0;
+}
+
+//연결해제 메세지 설정
+void setDisconnectedMsg(char* msg, int clntNum) {
+	msg[0] = 'I';
+	msg[1] = 1;
+	msg[2] = clntNum;
+	msg[3] = 0;
+}
+
+//현재 방인원 정보 메세지 설정
+void setRoomInfoMsg(char* msg) {
+	msg[0] = 'J';
+	msg[1] = 2;
+	msg[2] = clntCntOfRoom[0];
+	msg[3] = clntCntOfRoom[1];
+	msg[4] = 0;
+}
+
+//게임이 종료됐는지 확인
+int isGameEnd(int roomNum) {
+	if (mafiaCnt[roomNum] == 0 || mafiaCnt[roomNum] >= citizenCnt[roomNum]) { //마피아가 다죽거나 마피아수가 시민수 이상인 경우
+		return TRUE;
+	}
+	else return FALSE;
+}
+
+//몇번방인지 확인
+int checkRoomNum(SOCKET clnt) {
+	int i;
+	int roomNum = 1;
+
+	for (i = 0; i < clntCntOfRoom[0]; i++) {
+		if (clnt == clntSocksOfRoom[0][i]) {
+			roomNum = 0;
+			break;
+		}
+	}
+
+	return roomNum;
+}
+
+//해당방에 입장해있는 클라이언트들에게 메세지 전송
+void sendMsgToAll(char* message, int roomNum, DWORD len) {
 	WSABUF wsaBuf;
 	LPPER_IO_DATA ioInfo;
-	int i, recvBytes, a;
-	
-	for (i = 0; i < clntCnt; i++) {
+	int i, recvBytes;
+
+	for (i = 0; i < MAX_PLAYER; i++) {
 		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 		ioInfo->wsaBuf.len = len;
 		ioInfo->wsaBuf.buf = message;
 		ioInfo->rwMode = WRITE;
-		WSASend(clntSocks[i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+		WSASend(clntSocksOfRoom[roomNum][i], &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
 	}
 }
 
+//에러처리
 void errorHandling(char* message) {
 	fputs(message, stderr);
 	fputc('\n', stderr);
